c6fe28fcbd723d0bc7c9efb8bee86ecc7d9298c4
Rafael <rafaelcastilho15@outlook.com>
2022-09-13 23:42:08 -0300

    Finalização algoritmo guloso

--- create_instace.py
+++ create_instace.py
@@ -1,10 +1,15 @@
+def create_random_matriz(nLins, nCols):
+    from random import randint
+    vals = [None] * nLins
+    for i in range(nLins):
+        vals[i] = [0] * nCols
+        for j in range(nCols):
+            vals[i][j] = randint(0, 10)
+    return vals
 
-
-
-
-
-matriz = create_random_matriz()
-file = open("myfile.txt", "w+")
+nLins = nCols = 5
+matriz = create_random_matriz(nLins, nCols)
+file = open("/home/rafael/alg/Projeto-de-Algoritmo/myfile.txt", "w+")
   
 content = str(matriz)
 file.write(content)
